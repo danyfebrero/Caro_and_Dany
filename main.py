09f79c81583e40982d6280e4cdd42ae048cb1b96
Daniel Jimenez <danyfebrero@gmail.com>
2022-09-22 23:10:02 -0400

    First commit fix

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 22 22:31:49 2022
+
+@author: Daniel
+"""
+
+def main():
+    
+    first_name = 'caroline'
+    
+    print(f"Sweet {first_name.title()}!")
+    
+    
+if __name__ == '__main__':
+    main()
